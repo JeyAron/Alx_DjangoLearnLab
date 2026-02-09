3937bd494906b9bc0976bfdf4b47418675d4b785
Aaron <aronsule@gmail.com>
2026-02-09 12:44:26 +0300

    Develop the ability to manage user authentication within a Django application. This task focuses on setting up user login, logout, and registration functionalities using Django’s built-in authentication system

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 from .views import (
     list_books,
     LibraryDetailView,
@@ -10,7 +12,7 @@ from .views import (
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", UserLoginView.as_view(), name="login"),
-    path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", register, name="register"),
 ]
