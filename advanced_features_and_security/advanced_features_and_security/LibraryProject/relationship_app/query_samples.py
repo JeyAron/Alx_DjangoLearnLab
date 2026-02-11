ed6b28df8b2886f87f9f52de268cff26a144aef4
Aaron <aronsule@gmail.com>
2026-02-11 09:10:47 +0300

    Customize Django’s user model to suit the specific needs of your application, demonstrating an understanding of extending Django’s authentication system

---
+++ advanced_features_and_security/advanced_features_and_security/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,35 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Required variables
+author_name = "George Orwell"
+library_name = "Central Library"
+
+# Create sample data
+author, _ = Author.objects.get_or_create(name=author_name)
+
+book1, _ = Book.objects.get_or_create(title="1984", author=author)
+book2, _ = Book.objects.get_or_create(title="Animal Farm", author=author)
+
+library, _ = Library.objects.get_or_create(name=library_name)
+library.books.add(book1, book2)
+
+librarian, _ = Librarian.objects.get_or_create(
+    name="Alice",
+    library=library
+)
+
+# ------------------------
+# REQUIRED QUERY PATTERNS
+# ------------------------
+
+# 1. Query all books by a specific author (ForeignKey)
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+
+# 2. List all books in a library (ManyToMany)
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+
+# 3. Retrieve the librarian for a library (OneToOne)
+librarian = Librarian.objects.get(library=library)
+
