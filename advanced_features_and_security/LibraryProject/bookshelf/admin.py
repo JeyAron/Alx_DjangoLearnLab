6e889457e7a88308f8fc2e43d4a920220d282669
Aaron <aronsule@gmail.com>
2026-02-17 14:18:10 +0300

    Customize Django’s user model to suit the specific needs of your application, demonstrating an understanding of extending Django’s authentication system

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from .models import Book
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 @admin.register(Book)
@@ -7,3 +9,21 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
     list_filter = ('publication_year',)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
