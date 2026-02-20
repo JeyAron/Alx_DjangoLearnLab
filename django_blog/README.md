57dd8966e86fe3407b0eeb691972bf8bb06b8ddd
Aaron <aronsule@gmail.com>
2026-02-20 18:40:40 +0300

    Develop and integrate features in your Django blog project that allow users to create, read, update, and delete (CRUD) blog posts. This will enable authors to manage their content dynamically and viewers to browse blog entries effectively

---
+++ django_blog/README.md
@@ -0,0 +1,7 @@
+Blog Post Management
+====================
+- ListView: /posts/
+- DetailView: /posts/<id>/
+- CreateView: /posts/new/ (authenticated)
+- UpdateView: /posts/<id>/edit/ (author only)
+- DeleteView: /posts/<id>/delete/ (author only)
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
 def register_view(request):
@@ -31,3 +35,47 @@ def profile_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+# List all posts (public)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # your template path
+    context_object_name = 'posts'
+    ordering = ['-published_date']  # newest first
+
+# Detail of a single post (public)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# Create a post (authenticated)
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        # Set logged-in user as author
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Update a post (author only)
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author  # Only author can edit
+
+# Delete a post (author only)
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')  # redirect after deletion
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
