2ae100d73d1063b7db15eb46c2be7a782f179700
Aaron <aronsule@gmail.com>
2026-02-20 15:43:25 +0300

    Enhance the usability and functionality of your API by implementing filtering, searching, and ordering capabilities. This task focuses on providing users with the tools to easily access and manipulate the data presented through your API

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
