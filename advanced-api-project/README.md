c3db03a893ea41284d24003ad2523573faaebd75
Aaron <aronsule@gmail.com>
2026-02-20 15:35:35 +0300

    Enhance the usability and functionality of your API by implementing filtering, searching, and ordering capabilities. This task focuses on providing users with the tools to easily access and manipulate the data presented through your API

--- advanced-api-project/README.md
+++ advanced-api-project/README.md
@@ -27,3 +27,26 @@ GET     /api/books/<id>/
 POST    /api/books/create/
 PUT     /api/books/<id>/update/
 DELETE  /api/books/<id>/delete/
+
+# Advanced Query Capabilities
+
+This API supports:
+
+1. Filtering
+   - ?title=
+   - ?publication_year=
+   - ?author=
+
+2. Search
+   - ?search=<keyword>
+   Searches title and author name.
+
+3. Ordering
+   - ?ordering=title
+   - ?ordering=-publication_year
+
+Filtering backend:
+- DjangoFilterBackend
+- SearchFilter
+- OrderingFilter
+
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
 import datetime
@@ -13,6 +15,11 @@ class BookListView(generics.ListAPIView):
     GET /books/
     Returns a list of all books.
 
+    Advanced Features:
+    - Filtering by title, author, publication_year
+    - Searching by title and author name
+    - Ordering by title and publication_year
+
     Permissions:
     - Read-only access for everyone.
     """
@@ -20,6 +27,13 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['title', 'publication_year', 'author']
+
+    search_fields = ['title', 'author__name']
+
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
 
 class BookDetailView(generics.RetrieveAPIView):
     """
