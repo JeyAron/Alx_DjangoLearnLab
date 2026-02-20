e76d253035b7a9681897085825886c74cc055738
Aaron <aronsule@gmail.com>
2026-02-20 13:41:19 +0300

    Secure your API endpoints by implementing various authentication schemes and permission settings in Django REST Framework. This task will ensure that only authorized users can access and modify data through your API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Book
 from .serializers import BookSerializer
 
@@ -12,3 +13,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
